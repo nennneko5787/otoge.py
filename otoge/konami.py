80155379c8606b6a69ccb3e59c4e868033359132
Nennneko5787 <nennneko5787@gmail.com>
2024-12-28 12:49:35 +0900

    headless

--- otoge/konami.py
+++ otoge/konami.py
@@ -87,7 +87,7 @@ class KonamiCaptcha:
         options.add_argument(
             "--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36"
         )
-        # options.add_argument("--headless")
+        options.add_argument("--headless")
         options.add_argument("--disable-blink-features=AutomationControlled")
         service = Service(log_path=os.devnull)
 
