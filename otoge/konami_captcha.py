8d7af0e73c5b6ad775d29b793dfcefbfce89e5f9
Nennneko5787 <nennneko5787@gmail.com>
2025-01-04 10:27:11 +0900

    !!

--- otoge/konami_captcha.py
+++ otoge/konami_captcha.py
@@ -103,6 +103,8 @@ class KonamiCaptcha:
         options.add_argument("--log-level=0")
         options.add_argument("--headless")
         options.add_argument("--disable-blink-features=AutomationControlled")
+        options.add_argument("--disable-dev-shm-usage")
+        options.add_argument("--no-sandbox")
         service = Service(log_path=os.devnull)
 
         self.driver = webdriver.Chrome(service=service, options=options)
